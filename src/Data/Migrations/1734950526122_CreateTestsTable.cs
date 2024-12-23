323702fcea74b1571f3c675b58536b5f78542c27
Eudora (Jitera) <developers@jitera.com>
2024-12-23 10:43:30 +0000

    feat[database]: create table 'tests' Changes * '/src/Core/Entities/Test.cs': created * '/src/Data/Migrations/1734950526122_CreateTestsTable.cs': created * '/src/Data/Migrations/ConduitContextModelSnapshot.cs': created

---
+++ src/Data/Migrations/1734950526122_CreateTestsTable.cs
@@ -0,0 +1,32 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Realworlddotnet.Data.Migrations
+{
+    public partial class CreateTestsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tests", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tests");
+        }
+    }
+}
