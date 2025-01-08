1275fed1bb775563fe4169ecb5b57c7471518aa3
Didi (Jitera) <developers@jitera.com>
2025-01-08 09:14:22 +0000

    feat[database]: create table 'tests' Changes * '/src/Core/Entities/Test.cs': created * '/src/Data/Migrations/1736327636994_AddTestsTable.cs': created

---
+++ src/Data/Migrations/1736327636994_AddTestsTable.cs
@@ -0,0 +1,32 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Realworlddotnet.Data.Migrations
+{
+    public partial class AddTestsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tests", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tests");
+        }
+    }
+}
