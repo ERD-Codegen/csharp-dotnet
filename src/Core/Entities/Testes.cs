6ea891543cbae6992eec6a5b20268f5ad859db9f
Claudetta (Jitera) <developers@jitera.com>
2025-01-08 09:13:57 +0000

    feat[database]: create table 'testes' Changes * '/src/Core/Entities/Testes.cs': created * '/src/Data/Migrations/1736327620796_CreateTestesTable.cs': created

---
+++ src/Core/Entities/Testes.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Realworlddotnet.Core.Entities;
+
+public class Testes
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
